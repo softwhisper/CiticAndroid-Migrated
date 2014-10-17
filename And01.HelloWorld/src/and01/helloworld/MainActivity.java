86bcde03b814aa97a66c9871f78d8a0631fefa30
Pablo Formoso Estrada <pablo@pabloformoso.com>
2014-10-17 14:30:30 +0200

    dia2

--- And01.HelloWorld/src/and01/helloworld/MainActivity.java
+++ And01.HelloWorld/src/and01/helloworld/MainActivity.java
@@ -1,20 +1,74 @@
 package and01.helloworld;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import and01.helloworld.SecondActivity;
 
 public class MainActivity extends ActionBarActivity {
-
+	private static String TAG = "MainActivity";
+	private Button button; 	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	Log.d(TAG, "onCreate");
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        button = (Button)findViewById(R.id.button1);        
+        button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+				startActivity(intent);
+			}
+		});
     }
 
-
+    
+	@Override
+	protected void onStart() {
+		Log.d(TAG, "onStart");	
+		super.onStart();
+	}
+	
+	@Override
+	protected void onResume() {
+		Log.d(TAG, "onResume");
+		super.onResume();
+	}
+	
+	// Destrucción
+	
+	@Override
+	protected void onPause() {
+		Log.d(TAG, "onPause");
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop() {
+		Log.d(TAG, "onPause");
+		super.onStop();
+	}	
+	
+	@Override
+	protected void onDestroy() {
+		Log.d(TAG, "onDestroy");
+		super.onDestroy();
+	}    
+    
+    
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
