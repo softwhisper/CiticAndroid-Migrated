e1fffba58b6ab5ed27ab446274ad6c1da2fc6ddb
Pablo Formoso Estrada <pablo@pabloformoso.com>
2014-10-17 09:57:29 +0200

    And01

---
+++ And05.FragmentDialog/gen/curso/and05/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package curso.and05;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ And06.FragmentList/gen/curso/and06/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package curso.and06;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
